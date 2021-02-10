34deeb89e2eec9e62750f2f74f2459e23b704d14
majejam <t.f.m.lacroix@gmail.com>
2021-02-10 09:39:15 +0100

    :sparkles: added unity integration

---
+++ pages/jeu.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <unity
+      src="Game/Build/build.json"
+      width="1000"
+      height="600"
+      unityLoader="Game/Build/UnityLoader.js"
+    ></unity>
+  </div>
+</template>
+
+<script>
+import Unity from 'vue-unity-webgl'
+export default {
+  components: { Unity },
+}
+</script>
+
+<style lang="scss">
+.container {
+  overflow-x: hidden;
+}
+</style>
